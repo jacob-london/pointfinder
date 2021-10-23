49234ea2da042e7e06701c47c413c2f8106b6197
Jacob London <73508509+jacob-london@users.noreply.github.com>
2021-10-23 10:08:40 -0400

    Add files via upload

--- slopeinterceptpointfinder.py
+++ slopeinterceptpointfinder.py
@@ -1,15 +1,17 @@
 m = input("Enter Your Slope: ")
 b = input("Enter Your Y-Intercept: ")
 x = input("Enter Starting Value of X: ")
+z = input("Enter the Increment You Would Like to Count By")
 m = float(m)
 b = float(b)
 x = float(x)
+z = float(z)
 print(m * x + b)
-x = x+1
+x = x+z
 print(m * x +b)
-x = x+1
+x = x+z
 print(m * x + b)
-x = x+1
+x = x+z
 print(m * x + b)
-x = x+1
+x = x+z
 print(m * x + b)
