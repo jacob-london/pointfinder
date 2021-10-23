a60c9a566e3cdb2618f36666813f614c03c152b6
Jacob London <73508509+jacob-london@users.noreply.github.com>
2021-10-23 09:58:33 -0400

    Add files via upload

---
+++ slopeinterceptpointfinder.py
@@ -0,0 +1,15 @@
+m = input("Enter Your Slope: ")
+b = input("Enter Your Y-Intercept: ")
+x = input("Enter Starting Value of X: ")
+m = float(m)
+b = float(b)
+x = float(x)
+print(m * x + b)
+x = x+1
+print(m * x +b)
+x = x+1
+print(m * x + b)
+x = x+1
+print(m * x + b)
+x = x+1
+print(m * x + b)
